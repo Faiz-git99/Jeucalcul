ca4a71e80500180a5e7d7ef2ac349dc33189b2a9
Faiz-git99 <marifaiz97@gmail.com>
2024-10-24 00:07:02 +0300

    calcul automatique de lage

--- jeucalcul.js
+++ jeucalcul.js
@@ -6,7 +6,7 @@ let AnneeNaissance = document.querySelector("#dateNaissance");
  // la fonction calculAge qui va peremttre de faire le calcul pour ensuite nous retourner le résultat 
 function calculAge(anneeEnCours, datedeNaissance){
 let age = anneeEnCours - datedeNaissance;
-console.log("age calculé :", age);
+console.log("Age calculé :", age);
 return age;
 
 }
@@ -18,8 +18,13 @@ console.log(monNom);
 let monPrenom = document.querySelector("#prenom");
 console.log(monPrenom);
 
-let maSituation = document.querySelector("#salarie");
-console.log(maSituation);
+// Récupère l'année automatiquement pour l'afficher sur le footer
+let recupAnnee = document.querySelector("#afficheAnnee")
+const date = new Date();
+recupAnnee.innerHTML += date.getFullYear();
+
+let elementSpan = document.querySelector("#affichage");
+
 
 // Déclarer un bouton qui permet d'actionner un mouvement
 let elementBoutonValider = document.querySelector("#ageAuto");
@@ -38,13 +43,35 @@ let saisirSonPrenom = monPrenom.value;
 console.log("Mon prénom est :", saisirSonPrenom);
 
 // On récupère seulement la valeur qui se trouve dans l'input 'situation sociale' après avoir cliquer sur le bouton 'valider'
-let mySituation = maSituation.value;
-console.log("Je suis ", maSituation);
+const elementsituation = document.querySelectorAll('input[name="situation"]');
+console.log(elementsituation);
+//console.log(elementsituation[0].checked);
+//console.log(elementsituation[1].checked);
 
-// calcul pour récuperer l'âge auto. et afficher le résultat après avoir cliqué le bouton 'valider'
+let situationCochee;
+for(let i = 0; i < elementsituation.length; i++){
+
+   if(elementsituation[i].checked){
+      console.log(elementsituation[i].value);
+      situationCochee = elementsituation[i].value;
+      break
+   }
+
+}
+elementSpan.innerHTML += `Vous êtes ${situationCochee}.`;
+// calcul pour récuperer l'année et afficher le résultat après avoir cliqué le bouton 'valider'
 let AnneeSaisie = AnneeNaissance.value;
-console.log("Année saisie :", AnneeSaisie);
+console.log("Année naissance :", AnneeSaisie);
 
-calculAge( 2024, AnneeSaisie);
+//récupérer l'âge automatiquement et le message 
+let age = calculAge(2024, AnneeSaisie);
+ elementSpan.innerHTML += `vous avez ${age} ans.`; /* inner.HTML += veut dire d'insérer le message dans cette balise HTML */
 
+ // afficher la condition si on est majeur ou mineur (règle 3 et 4)
+ if(age > 18){
+   elementSpan.innerHTML += `Vous êtes majeur.`;
+ } else{ // avec else, on ne déclare plus la condition
+   elementSpan.innerHTML += `Vous êtes mineur.`;
+ }
+  
 });
